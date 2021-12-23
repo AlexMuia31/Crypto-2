94c9ec25418f06c9b2406469c6ccc07858cfd59e
AlexMuia31 <alexmuia31@gmail.com>
2021-12-23 11:50:39 +0300

    basic set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header/Header';
+import Homepage from './Pages/Homepage/Homepage';
+import CoinPage from './Pages/CoinPage/CoinPage';
+import { useStyles } from './AppStyles';
 
 
 function App() {
+  const classes = useStyles()
+
+
   return (
-    <BrowserRouter>
-      <div>
+    <Router>
+      <div className={classes.App}>
         <Header />
+        <Routes>
+          <Route path='/' exact element={<Homepage />} />
+          <Route path='/coins/:id' element={<CoinPage />} />
+        </Routes>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
+import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <AppBar color='transparent' position='static'>
+            <Container>
+                <Toolbar>
+                    <Typography>
+                        CryptoRank2
+                    </Typography>
+                    <Select>
+                        <MenuItem>USD</MenuItem>
+                        <MenuItem>Ksh</MenuItem>
+                    </Select>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
