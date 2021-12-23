86fb2d91457bd1acf6a48530ad5d3a2f93c58f27
AlexMuia31 <alexmuia31@gmail.com>
2021-12-23 21:05:25 +0300

    AppBar and its dark theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,49 @@
-import { AppBar, Container, MenuItem, Select, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Container, createTheme, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
 import React from 'react'
+import { useStyles } from './HeaderStyles'
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
-    return (
-        <AppBar color='transparent' position='static'>
-            <Container>
-                <Toolbar>
-                    <Typography>
-                        CryptoRank2
-                    </Typography>
-                    <Select>
-                        <MenuItem>USD</MenuItem>
-                        <MenuItem>Ksh</MenuItem>
-                    </Select>
+    const classes = useStyles()
+    const navigate = useNavigate()
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: '#fff',
+            },
+            type: 'dark',
+        }
+    })
 
-                </Toolbar>
-            </Container>
-        </AppBar>
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar color='transparent' position='static'>
+                <Container>
+                    <Toolbar>
+                        <Typography onClick={() => navigate('/')}
+                            className={classes.title}
+                            variant='h6'
+                        >
+                            CryptoRank2
+                        </Typography>
+                        <Select
+                            variant='outlined'
+                            style={{
+                                width: 100,
+                                height: 40,
+                                marginRight: 15,
+                            }}
+                        >
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"KSH"}>KSH</MenuItem>
+                        </Select>
+
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
     )
 }
 
---
+++ src/components/Header/HeaderStyles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+
+export const useStyles = makeStyles(() => ({
+    title: {
+        flex: 1,
+        color: 'gold',
+        fontFamily: 'Nunito',
+        fontWeight: 'bold',
+        cursor: 'pointer',
+    }
+}))
