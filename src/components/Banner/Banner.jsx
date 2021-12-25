7a2c74b7016250d8ac202dbc6225a842836b03fa
AlexMuia31 <alexmuia31@gmail.com>
2021-12-25 16:01:22 +0300

    banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import { Container, Typography } from '@material-ui/core'
+import React from 'react'
+import { bannerStyles } from './BannerStyles'
+
+
+const Banner = () => {
+    const classes = bannerStyles()
+    return (
+        <div className={classes.banner}>
+            <Container className={classes.bannerContent}>
+                <div className={classes.tagline}>
+                    <Typography
+                        variant='h2'
+                        style={{
+                            fontWeight: 'bold',
+                            marginBottom: 15,
+                            fontFamily: 'Nunito'
+                        }}
+                    >
+                        Crypto Rank
+                    </Typography>
+                    <Typography
+                        variant='subtitle1'
+                        style={{
+                            color: 'darkgrey',
+                            textTransform: 'capitalize',
+                            fontFamily: 'Nunito'
+                        }}
+                    >
+                        Providing all info about cryptocurrencies
+                    </Typography>
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/components/Banner/BannerStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+
+export const bannerStyles = makeStyles(() => ({
+    banner: {
+        backgroundImage: 'url(./crypto4.jpeg)',
+
+    },
+    bannerContent: {
+        height: 400,
+        display: 'flex',
+        flexDirection: 'column',
+        paddingTop: 25,
+        justifyContent: 'space-around',
+    },
+    tagline: {
+        display: 'flex',
+        height: "40%",
+        flexDirection: 'column',
+        justifyContent: 'center',
+        textAlign: 'center',
+    }
+}))
